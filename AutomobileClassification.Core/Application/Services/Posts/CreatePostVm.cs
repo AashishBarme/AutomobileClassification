d5406ce8824e04a0ddd5c57e085d4fb4f3d735c7
AashishBarme <aashish.barme9@gmail.com>
2021-02-11 20:24:33 +0545

    post required vm added

---
+++ AutomobileClassification.Core/Application/Services/Posts/CreatePostVm.cs
@@ -0,0 +1,13 @@
+namespace AutomobileClassification.Core.Application.Services.Posts
+{
+    public class CreatePostVm
+    {
+        public int Id {get; set;}
+        public string Image {get; set;}
+        public string Title {get; set;}
+        public string Url {get; set;}
+        public int CategoryId {get; set;}
+        public int ModelId {get; set;}
+        public int UserId {get; set;}
+    }
+}
---
+++ AutomobileClassification.Core/Application/Services/Posts/PostListVm.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+namespace AutomobileClassification.Core.Application.Services.Posts
+{
+    public class PostListVm
+    {
+       public IList<PostListDto> Posts {get; set;}
+    } 
+
+
+    public class PostListDto
+    {
+       public int Id {get; set;}
+       public string Title {get; set;}
+       public string Image {get; set;}
+       public string Category {get; set;}
+       public string Model {get; set;}
+       public int PostLikeCount {get; set;}
+    }
+}
