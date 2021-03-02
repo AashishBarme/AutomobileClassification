642ac7c522af734f42dee13beebca3d2a544d418
AashishBarme <aashish.barme9@gmail.com>
2021-03-02 20:33:42 +0545

    migration updated

--- AutomobileClassification.Core/Migrations/20210302135344_InitialCreate.Designer.cs
+++ AutomobileClassification.Core/Migrations/20210302135344_InitialCreate.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using AutomobileClassification.Core.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace AutomobileClassification.Core.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210302135344_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
